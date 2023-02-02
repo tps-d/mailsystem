08a4676c34e31ec5fd4e6244e1ec862ec57fe0c3
phoenix-pwf <109668346+phoenix-pwf@users.noreply.github.com>
2023-02-02 11:45:23 +0300

    1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,7 +36,7 @@ class DatabaseSeeder extends Seeder
                 ]);
 
         DB::table('workspace_users')->insert([
-                   'owner_id' => 1,
+                   'workspace_id' => 1,
                    'user_id' => 1,
                     'role' => 'owner',
                     'created_at' => now(),
