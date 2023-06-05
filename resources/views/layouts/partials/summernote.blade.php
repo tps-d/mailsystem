63f7a4a1636e9b89aedc73ceacea0e4b49934303
phoenix-pwf <109668346+phoenix-pwf@users.noreply.github.com>
2023-06-05 15:27:42 +0300

    1

--- resources/views/layouts/partials/summernote.blade.php
+++ resources/views/layouts/partials/summernote.blade.php
@@ -1,11 +1,12 @@
 @push('css')
 
-    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote-bs4.min.css" rel="stylesheet">
+
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+   
 @endpush
 
 @push('js')
-
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote-bs4.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 
     <script>
         $(function () {
